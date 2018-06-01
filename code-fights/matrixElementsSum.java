714f9deb1caf65e08bd8a113a460d1d5af9b1f13
Daniel Brito <daniell-henrique@hotmail.com>
2018-06-01 12:09:08 -0300

    Add files via upload

---
+++ code-fights/matrixElementsSum.java
@@ -0,0 +1,38 @@
+/* https://codefights.com/arcade/intro/level-2/xskq4ZxLyqQMCLshr */
+
+public class matrixElementsSum {
+
+	public static void main(String[] args) {
+		
+		int[][] matrix = {{0,1,1,2},
+		    	          {0,5,0,0},
+				          {2,0,3,3}};
+		
+		int row = matrix.length;
+		int col = matrix[0].length;
+		
+		int i=0, j=0, total=0, count=0;
+		
+		for(j=0; j<col; j++) {
+			
+			count = 0;
+			
+			for(i=0; i<row; i++) {
+				
+				if(matrix[i][j]==0) {
+					
+					count++;
+				}
+				else {
+					
+					if(count==0) {
+						
+						total += matrix[i][j];
+					}
+				}
+			}
+		}
+		
+		System.out.println(total);
+	}
+}
