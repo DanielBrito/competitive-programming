4cce60192b55cbea379093599d30f71c8da0039d
Daniel Brito <daniell-henrique@hotmail.com>
2018-06-01 11:09:04 -0300

    Add files via upload

---
+++ code-fights/almostIncreasingSequence.java
@@ -0,0 +1,36 @@
+/* https://codefights.com/arcade/intro/level-2/2mxbGwLzvkTCKAJMG/solutions */
+
+public class almostIncreasingSequence {
+
+	public static void main(String[] args) {
+		
+		int[] sequence = {1,2,3,4,3,6};
+		
+		int equalGreater=0, postConsecutive=0, i;
+		
+		for(i=0; i<sequence.length-1; i++) {
+		
+			if(sequence[i]>=sequence[i+1]) {
+						
+				equalGreater++;
+			}
+		}
+		
+		for(i=0; i<sequence.length-2; i++) {
+			
+			if(sequence[i]>=sequence[i+2]) {
+						
+				postConsecutive++;
+			}
+		}
+		
+		if(equalGreater+postConsecutive>2) {
+			
+			System.out.println("False");
+		}
+		else {
+			
+			System.out.println("True");			
+		}
+	}
+}
