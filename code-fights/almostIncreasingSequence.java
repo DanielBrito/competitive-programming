386496a9e5703cf76b7e821fd62109c5531d3882
Daniel Brito <daniell-henrique@hotmail.com>
2018-06-01 20:33:47 -0300

    Update almostIncreasingSequence.java

--- code-fights/almostIncreasingSequence.java
+++ code-fights/almostIncreasingSequence.java
@@ -1,4 +1,4 @@
-/* https://codefights.com/arcade/intro/level-2/2mxbGwLzvkTCKAJMG/solutions */
+/* https://codefights.com/arcade/intro/level-2/2mxbGwLzvkTCKAJMG */
 
 public class almostIncreasingSequence {
 
