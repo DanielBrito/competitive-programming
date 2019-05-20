d1b134cc318f86f074d06a5a7c028e8a7faee8c8
Daniel Brito <daniell-henrique@hotmail.com>
2019-05-20 00:39:26 -0300

    Add files via upload

---
+++ CodeSignal/ArrayChange.java
@@ -0,0 +1,24 @@
+/* https://app.codesignal.com/arcade/intro/level-4/xvkRbxYkdHdHNCKjg */
+
+public class ArrayChange {
+
+    public static void main(String[] args) {
+        
+        int[] array = {3122, -645, 2616, 13213, -8069};
+        int sum=0, temp;
+        
+        for(int i=0; i<array.length-1; i++) {
+            
+            if(array[i+1]<=array[i]) {
+                
+                temp = array[i+1];
+                
+                array[i+1] = array[i+1]+((Math.abs(array[i]-array[i+1]))+1);
+                
+                sum = sum + Math.abs(array[i]-temp)+1;
+            }
+        }
+        
+        System.out.println(sum);
+    }
+}
