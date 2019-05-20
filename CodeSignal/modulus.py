24e206d61c3cc6f7bd33a6c5ea5f82830d455de2
Daniel Brito <daniell-henrique@hotmail.com>
2019-05-20 01:16:45 -0300

    Add files via upload

---
+++ CodeSignal/modulus.py
@@ -0,0 +1,9 @@
+# https://app.codesignal.com/arcade/python-arcade/meet-python/mygD2J9yDbRmtKW8T
+
+def modulus(n):
+    if isinstance(n, int):
+        return n % 2
+    else:
+        return -1
+
+print(modulus(15))
