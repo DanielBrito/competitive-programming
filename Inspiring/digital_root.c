133db1efe5684405812594fc78b76b6e09fac44b
Daniel Brito <daniell-henrique@hotmail.com>
2019-06-27 03:12:41 -0300

    Update digital_root.c

--- Inspiring/digital_root.c
+++ Inspiring/digital_root.c
@@ -1,5 +1,7 @@
 /* By: Unnamed, zoqdupt, monadius, whatmename, DarkD1 (plus 4 more warriors) @ Codewars */
 
+/* Proof: https://proofwiki.org/wiki/Existence_of_Digital_Root */
+
 #include<stdio.h>
 
 int digital_root(int n);
