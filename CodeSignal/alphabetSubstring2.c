8ae7e6d76743259e061211c6fb23e3766022abe0
Daniel Brito <daniell-henrique@hotmail.com>
2019-06-28 23:26:00 -0300

    5 lines & 130 chars -> 1line + 80 chars

---
+++ CodeSignal/alphabetSubstring2.c
@@ -0,0 +1,21 @@
+/* https://app.codesignal.com/challenge/ptFrnXhzvADkCz5Ls */
+
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+bool alphabetSubstring(char* s);
+
+int main(){
+	
+	char string[20] = "efghi";
+	
+	printf("%s\n", alphabetSubstring(string) ? "true" : "false");
+	
+	return 0;
+}
+
+bool alphabetSubstring(char* s){
+    
+    return strstr("abcdefghijklmnopqrstuvwxyz", s) ? true : false;
+}
