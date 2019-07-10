e8a314b654b5d2b2fe8d5246e3c7c424bacfc43f
Daniel Brito <daniell-henrique@hotmail.com>
2019-07-09 23:40:39 -0300

    Update simpleArraySum.js

--- HackerRank/simpleArraySum.js
+++ HackerRank/simpleArraySum.js
@@ -1,7 +1,5 @@
 /* https://www.hackerrank.com/challenges/simple-array-sum/problem */
 
-/* https://codeburst.io/javascript-arrays-finding-the-minimum-maximum-sum-average-values-f02f1b0ce332 */
-
 function simpleArraySum(ar) {
     return ar.reduce((accumulator, value) => accumulator + value, 0)
 }
