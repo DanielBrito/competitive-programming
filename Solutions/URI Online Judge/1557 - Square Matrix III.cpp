8ef29a96dee8f2137b4875a928ec1fefa99b8d97
Daniel <daniell-henrique@hotmail.com>
2020-06-07 22:23:49 -0300

    Corrigindo Presentation Error

--- Solutions/URI Online Judge/1557 - Square Matrix III.cpp
+++ Solutions/URI Online Judge/1557 - Square Matrix III.cpp
@@ -6,9 +6,9 @@
 
 using namespace std;
 
-int countDigits(int n){
+unsigned long long int countDigits(unsigned long long int n){
 
-    int count = 0;
+    unsigned long long int count = 0;
 
     while(n>9){
 
@@ -23,27 +23,18 @@ int main(){
 
     vector<int> dim;
     bool finish = false;
-    int N, v, i, j, k, max, spaces;;
+    unsigned long long int v, i, j, k, max, spaces;
+    int n, c;
 
-    while (!finish){
-
-        cin >> N;
-
-        if(N!=0) dim.push_back(N);
-        else finish = true;
+    while (cin >> n && n!=0){
+        
+        dim.push_back(n);
     }
 
     for (v=0; v<dim.size(); v++){
 
         // Find max number of the matrix:
-
-        for (i = 1; i < pow(2, dim[v]); i *= 2){
-
-            for (j = i; j < pow(2, dim[v]) * i; j *= 2){
-
-                max = j;
-            }
-        }
+        max = pow(2,(2*(dim[v]-1)));
 
         spaces = countDigits(max);
 
@@ -51,11 +42,18 @@ int main(){
 
         for (i = 1; i < pow(2, dim[v]); i *= 2){
 
+            c = 0;
+
             for (j = i; j < pow(2, dim[v]) * i; j *= 2){
 
-                for(k=0; k<spaces-countDigits(j); k++) printf(" ");
+                for(k=0; k<spaces-countDigits(j); k++) {
 
-                cout << j << " ";
+                    printf(" ");
+                }
+
+                (c<(dim[v]-1)) ? cout << j << " " : cout << j;
+
+                c++;
             }
 
             cout << "\n";
