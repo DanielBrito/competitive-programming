3cd47eb1d3a1b94892cd56f8b5b716ceed861e5e
Daniel <daniell-henrique@hotmail.com>
2020-07-20 12:32:05 -0300

    Practicing TypeScript - Assert

--- Solutions/CheckiO/multTwo.ts
+++ Solutions/CheckiO/multTwo.ts
@@ -1,5 +1,14 @@
+const assert = require('assert').strict;
+
 function multTwo(a: number, b: number): number {
     return a*b;
 }
 
+console.log('Example:');
 console.log(multTwo(3, 2));
+
+// These "asserts" are used for self-checking
+assert.equal(multTwo(3, 2), 6);
+assert.equal(multTwo(0, 1), 0);
+
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
