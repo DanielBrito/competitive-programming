ff9ad5fa4f6fa088b122ec0de5f8dcb07b5dae2d
danielbrito <daniell-henrique@hotmail.com>
2020-11-22 13:31:07 -0300

    Warming up

---
+++ Solutions/HackerRank/FizzBuzz.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+/*
+ * Complete the 'fizzBuzz' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+void fizzBuzz(int n) {
+    
+    for(int i=1; i<=n; i++){
+        
+        if(i%3==0 && i%5==0){
+        
+            cout << "FizzBuzz\n";
+        }
+        else
+        if(i%3==0 && i%5!=0){
+            
+            cout << "Fizz\n";
+        }
+        else
+        if(i%5==0 && i%3!=0){
+            
+            cout << "Buzz\n";
+        }
+        else{
+            
+            cout << i << "\n";
+        }        
+    }
+}
+
+int main(){
+	
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    fizzBuzz(n);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
