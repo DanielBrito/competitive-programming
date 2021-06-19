2d4a8b7cc6ee91c9a3a84233defe8a069390aaf5
danielbrito <daniell-henrique@hotmail.com>
2021-06-19 16:25:14 -0300

    Staircase - HackerRank

---
+++ Solutions/HackerRank/staircase.js
@@ -0,0 +1,25 @@
+function staircase(n) {
+  let count = 1;
+  let answer = "";
+  
+  for(let i=0; i<n; i++){
+
+    for(let j=0; j<n-count; j++){
+
+      answer += " ";
+    }
+
+    for(let j=0; j<count; j++){
+
+      answer += "#";
+    }
+
+    ++count;
+
+    answer += "\n";
+  }
+  
+  console.log(answer);
+}
+
+staircase(6);
