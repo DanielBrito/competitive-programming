7b366595b88f1a69ed0d5c1c8f67b4cc8ec90bfe
danielbrito <daniell-henrique@hotmail.com>
2022-01-01 13:55:49 -0300

    HackerRank challenge

---
+++ Solutions/HackerRank/BirthdayCakeCandles.js
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+const candles = [4, 4, 1, 3];
+
+function birthdayCakeCandles(candles) {
+  const maxValue = Math.max(...candles);
+  return candles.filter((value) => value === maxValue).length;
+}
+
+console.log(birthdayCakeCandles(candles));
