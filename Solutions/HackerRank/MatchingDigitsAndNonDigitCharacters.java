f650d15471f06b5a2b356fe62460f7b2f2b7e034
Daniel Brito <daniell-henrique@hotmail.com>
2024-01-21 19:43:12 -0300

    Create MatchingDigitsAndNonDigitCharacters.java

---
+++ Solutions/HackerRank/MatchingDigitsAndNonDigitCharacters.java
@@ -0,0 +1,8 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\d{2}\\D\\d{2}\\D\\d{4}");
+    }
+}
