6c0664f6a4527df402b2499b9543a721e9cdb0f8
Daniel Brito <daniell-henrique@hotmail.com>
2024-01-21 19:47:04 -0300

    Create DrawingBook2.kt

---
+++ Solutions/HackerRank/DrawingBook2.kt
@@ -0,0 +1,45 @@
+import java.io.*
+import java.math.*
+import kotlin.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+fun pageCount(n: Int, p: Int): Int {    
+    var turningsLtoR = ceil((p-1)/2.0)
+    var turningsRtoL = if(n % 2 == 0) ceil((n-p)/2.0) else floor((n-p)/2.0)
+    
+    return min(turningsLtoR.toInt(), turningsRtoL.toInt())
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val p = readLine()!!.trim().toInt()
+
+    val result = pageCount(n, p)
+
+    println(result)
+}
