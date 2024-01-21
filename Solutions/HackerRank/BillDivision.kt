d4c3d2c4cfc5492cf56ae37c3c93be3ce103bd10
Daniel Brito <daniell-henrique@hotmail.com>
2024-01-21 19:47:55 -0300

    Create BillDivision.kt

---
+++ Solutions/HackerRank/BillDivision.kt
@@ -0,0 +1,54 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'bonAppetit' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY bill
+ *  2. INTEGER k
+ *  3. INTEGER b
+ */
+
+fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
+    var totalAnna = 0
+    
+    bill.forEachIndexed { index, element ->
+        if(index != k) totalAnna += element
+    }
+    
+    val totalSplit = totalAnna/2
+    
+    if(totalSplit == b) print("Bon Appetit")
+    else print ("${b-totalSplit}")
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val k = first_multiple_input[1].toInt()
+
+    val bill = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val b = readLine()!!.trim().toInt()
+
+    bonAppetit(bill, k, b)
+}
