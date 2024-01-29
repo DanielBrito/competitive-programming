58bebe7dcaa88b7bf8064394191c82d98e2fed5d
Daniel Brito <daniell-henrique@hotmail.com>
2024-01-29 18:53:53 -0300

    Create ElectronicsShop.kt

---
+++ Solutions/HackerRank/ElectronicsShop.kt
@@ -0,0 +1,49 @@
+import java.io.*
+import java.math.*
+import java.text.*
+import java.util.*
+import java.util.regex.*
+
+/*
+ * Complete the getMoneySpent function below.
+ */
+fun getMoneySpent(keyboards: Array<Int>, drives: Array<Int>, b: Int): Int {
+    var valueCombinations = mutableListOf<Int>()
+    
+    keyboards.forEach { keyboard ->
+        drives.forEach { drive ->
+            valueCombinations.add(keyboard + drive)            
+        }        
+    }
+    
+    val maxCombinedValue = valueCombinations.max()
+    val result = valueCombinations
+        .filter { it <= b && it <= maxCombinedValue }
+        .maxOrNull()
+        
+    return result ?: -1
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val bnm = scan.nextLine().split(" ")
+
+    val b = bnm[0].trim().toInt()
+
+    val n = bnm[1].trim().toInt()
+
+    val m = bnm[2].trim().toInt()
+
+    val keyboards = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val drives = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    /*
+     * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+     */
+
+    val moneySpent = getMoneySpent(keyboards, drives, b)
+
+    println(moneySpent)
+}
