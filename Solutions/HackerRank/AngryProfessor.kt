0751da01dce5d6655803b1dc49897cde1fe31aea
Daniel Brito <daniell-henrique@hotmail.com>
2024-02-15 22:28:43 -0300

    Create AngryProfessor.kt

---
+++ Solutions/HackerRank/AngryProfessor.kt
@@ -0,0 +1,52 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'angryProfessor' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY a
+ */
+
+fun angryProfessor(k: Int, a: Array<Int>): String {
+    val onTimeStudents = a.filter { it <= 0 }.size 
+    
+    return if(onTimeStudents >= k) "NO"
+    else "YES"
+}
+
+fun main(args: Array<String>) {
+    val t = readLine()!!.trim().toInt()
+
+    for (tItr in 1..t) {
+        val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+        val n = first_multiple_input[0].toInt()
+
+        val k = first_multiple_input[1].toInt()
+
+        val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+        val result = angryProfessor(k, a)
+
+        println(result)
+    }
+}
