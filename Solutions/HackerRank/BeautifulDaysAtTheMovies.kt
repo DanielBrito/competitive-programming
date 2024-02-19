df3833449085b3fd7d70fd63f73dc8fd6667284f
Daniel Brito <daniell-henrique@hotmail.com>
2024-02-19 13:20:01 -0300

    Create BeautifulDaysAtTheMovies.kt

---
+++ Solutions/HackerRank/BeautifulDaysAtTheMovies.kt
@@ -0,0 +1,55 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+
+fun beautifulDays(i: Int, j: Int, k: Int): Int {
+    var numberOfBeautifulDays = 0
+    
+    for(v in i..j) {
+        val reversed = v.toString().reversed().toInt()
+        val diff = Math.abs(v - reversed)
+        
+        if(diff.toDouble() % k.toDouble() == 0.0) numberOfBeautifulDays++
+    }
+    
+    return numberOfBeautifulDays
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val i = first_multiple_input[0].toInt()
+
+    val j = first_multiple_input[1].toInt()
+
+    val k = first_multiple_input[2].toInt()
+
+    val result = beautifulDays(i, j, k)
+
+    println(result)
+}
