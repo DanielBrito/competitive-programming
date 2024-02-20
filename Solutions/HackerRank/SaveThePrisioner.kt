c0e55bdb70a05c7927f4edf7508943c2c5e172e3
Daniel Brito <daniell-henrique@hotmail.com>
2024-02-19 23:11:46 -0300

    Create SaveThePrisioner.kt

---
+++ Solutions/HackerRank/SaveThePrisioner.kt
@@ -0,0 +1,53 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'saveThePrisoner' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. INTEGER s
+ */
+
+fun saveThePrisoner(n: Int, m: Int, s: Int): Int {  
+    val chair = (m + s - 1) % n
+    
+    return if(chair != 0) chair
+    else n
+}
+
+fun main(args: Array<String>) {
+    val t = readLine()!!.trim().toInt()
+
+    for (tItr in 1..t) {
+        val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+        val n = first_multiple_input[0].toInt()
+
+        val m = first_multiple_input[1].toInt()
+
+        val s = first_multiple_input[2].toInt()
+
+        val result = saveThePrisoner(n, m, s)
+
+        println(result)
+    }
+}
