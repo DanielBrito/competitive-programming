82cb994c27914a62194e5f57f7cf54937caba21d
Daniel Brito <daniell-henrique@hotmail.com>
2024-02-21 17:49:21 -0300

    Create JumpingOnTheCloudsRevisited.kt

---
+++ Solutions/HackerRank/JumpingOnTheCloudsRevisited.kt
@@ -0,0 +1,53 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the jumpingOnClouds function below.
+fun jumpingOnClouds(c: Array<Int>, k: Int): Int {
+    var currentPosition = 0
+    var energy = 100
+    val n = c.size
+    
+    do {
+        val nextPosition = (currentPosition + k) % n
+        
+        energy--
+        
+        if(c[nextPosition] == 1) energy -= 2
+        
+        currentPosition = nextPosition
+    } while(nextPosition != 0)
+    
+    return energy
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val nk = scan.nextLine().split(" ")
+
+    val n = nk[0].trim().toInt()
+
+    val k = nk[1].trim().toInt()
+
+    val c = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = jumpingOnClouds(c, k)
+
+    println(result)
+}
