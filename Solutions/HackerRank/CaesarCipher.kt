61071286b2a146cb182ca67e8491d451071186a4
Daniel Brito <daniell-henrique@hotmail.com>
2024-02-23 19:43:29 -0300

    Create CaesarCipher.kt

---
+++ Solutions/HackerRank/CaesarCipher.kt
@@ -0,0 +1,56 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'caesarCipher' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ */
+
+fun caesarCipher(s: String, k: Int): String {
+    val encrypted = StringBuilder()
+    
+    s.forEach { ch ->
+        if(ch.isLetter()) {
+            val base = if(ch.isLowerCase()) 97 else 65
+            val newCh = ((ch.code - base + k) % 26 + base).toChar()
+            
+            encrypted.append(newCh)
+        } else {
+            encrypted.append(ch)
+        }
+    }
+    
+    return encrypted.toString()
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val s = readLine()!!
+
+    val k = readLine()!!.trim().toInt()
+
+    val result = caesarCipher(s, k)
+
+    println(result)
+}
