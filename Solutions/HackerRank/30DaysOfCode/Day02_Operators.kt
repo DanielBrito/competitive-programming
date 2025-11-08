2d8232646ccfed4d75eeb46207a720f4d20eecef
Daniel Brito <daniell-henrique@hotmail.com>
2025-11-08 10:57:57 -0300

    Create Day02_Operators.kt

---
+++ Solutions/HackerRank/30DaysOfCode/Day02_Operators.kt
@@ -0,0 +1,46 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+import kotlin.math.*
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+fun solve(meal_cost: Double, tip_percent: Int, tax_percent: Int): Unit {
+    val tipAmount = meal_cost * (tip_percent / 100.0)
+    val taxAmount = meal_cost * (tax_percent / 100.0)
+    val totalCost = meal_cost + tipAmount + taxAmount
+    
+    println(totalCost.roundToInt())
+}
+
+fun main(args: Array<String>) {
+    val meal_cost = readLine()!!.trim().toDouble()
+
+    val tip_percent = readLine()!!.trim().toInt()
+
+    val tax_percent = readLine()!!.trim().toInt()
+
+    solve(meal_cost, tip_percent, tax_percent)
+}
