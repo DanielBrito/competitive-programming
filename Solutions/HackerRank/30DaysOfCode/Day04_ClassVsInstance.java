cbb311a3ccb2d11d4c964ce9433c81f614f0300f
Daniel Brito <daniell-henrique@hotmail.com>
2025-11-10 12:11:34 -0300

    Create Day04_ClassVsInstance.java

---
+++ Solutions/HackerRank/30DaysOfCode/Day04_ClassVsInstance.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Person {
+    private int age;
+
+    public Person(int initialAge) {
+        if (initialAge < 0) {
+            this.age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        } else {
+            this.age = initialAge;
+        }
+    }
+
+    public void amIOld() {
+        String message = "You are ";
+
+        if (this.age < 13) {
+            message += "young";
+        } else if (this.age >= 13 && this.age < 18) {
+            message += "a teenager";
+        } else {
+            message += "old";
+        }
+
+        System.out.printf("%s.\n", message);
+    }
+
+    public void yearPasses() {
+        this.age++;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+}
