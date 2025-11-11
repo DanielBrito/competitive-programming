ce8842b6589e62f38b2277170e9c09f8f5513f98
Daniel Brito <daniell-henrique@hotmail.com>
2025-11-11 09:06:32 -0300

    Create Day05_Loops.kt

---
+++ Solutions/HackerRank/30DaysOfCode/Day05_Loops.kt
@@ -0,0 +1,26 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+    
+    for(i in 1..10) println("$n x $i = ${i * n}")
+}
