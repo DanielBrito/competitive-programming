96313a9ac10b6b59132cf655ea8c81f2e3b88929
Daniel Brito <daniell-henrique@hotmail.com>
2025-11-13 09:17:25 -0300

    Create Day07_Arrays.kt

---
+++ Solutions/HackerRank/30DaysOfCode/Day07_Arrays.kt
@@ -0,0 +1,30 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    
+    for (i in n - 1 downTo 0) {
+        print("${arr[i]} ")
+    }
+}
