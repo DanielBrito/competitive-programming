cc24d4878222a4c2eb04722dfff82e7d87593f71
Daniel Brito <daniell-henrique@hotmail.com>
2025-11-19 18:05:01 -0300

    Create Day13_AbstractClasses.kt

---
+++ Solutions/HackerRank/30DaysOfCode/Day13_AbstractClasses.kt
@@ -0,0 +1,53 @@
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+    
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    abstract void display();
+}
+
+class MyBook extends Book {
+    int price;
+    
+    /**   
+    *   Class Constructor
+    *   
+    *   @param title The book's title.
+    *   @param author The book's author.
+    *   @param price The book's price.
+    **/
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+    
+    /**   
+    *   Method Name: display
+    *   
+    *   Print the title, author, and price in the specified format.
+    **/
+    @Override
+    void display() {
+        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d", this.title, this.author, this.price);
+    }
+}
+
+public class Solution {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
