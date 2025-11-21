61d4c0afb6b4cbda4772c65e193150b394af688e
Daniel Brito <daniell-henrique@hotmail.com>
2025-11-21 10:37:24 -0300

    Create Day15_LinkedList.java

---
+++ Solutions/HackerRank/30DaysOfCode/Day15_LinkedList.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+    public static  Node insert(Node head,int data) {
+        if(head == null) return new Node(data);
+        
+        Node newNode = new Node(data);
+        Node current = head;
+        
+        while(current.next != null) {
+            current = current.next;
+        }
+        
+        current.next = newNode;
+        
+        return head;
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
